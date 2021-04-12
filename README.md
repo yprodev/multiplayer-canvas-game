be37df23caeab44c398256705123bdbad09bae0a
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-04-12 21:08:26 +0300

    Chat created

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # multiplayer-canvas-game
 Workshop on multiplayer canvas game dev
+
+
+https://www.youtube.com/watch?v=NvkM1immvWo
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const staticPath = `${__dirname}/../client`;
+
+
+app.use(express.static(staticPath));
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (sock) => {
+	sock.emit('message', 'You are connected');
+	sock.on('message', text => io.emit('message', text));
+});
+
+server.on('error', (err) => {
+	console.error(err);
+});
+
+server.listen(8080, () => {
+	console.log('Server is ready');
+});
+
+
