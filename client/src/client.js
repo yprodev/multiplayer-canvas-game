b17ce4d12b7100f6562e5d6804a00e844cd26b1e
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-04-20 09:08:07 +0300

    Implement interaction with 'turn' event

--- client/src/client.js
+++ client/src/client.js
@@ -18,13 +18,49 @@ const onChatSubmitted = sock => e => {
 	sock.emit('message', text);
 };
 
+const getBoard = (canvas) => {
+	const ctx = canvas.getContext('2d');
+
+	const fillRect = (x, y, color) => {
+		ctx.fillStyle = color;
+		ctx.fillRect(x - 10, y - 10, 20, 20);
+	};
+
+	return {
+		fillRect
+	};
+};
+
+const getClickCoordinates = (element, event) => {
+	const { top, left } = element.getBoundingClientRect();
+	const { clientX, clientY } = event;
+
+	return {
+		x: clientX - left,
+		y: clientY - top
+	}
+};
+
 (() => {
+
+	const canvas = document.querySelector('canvas');
+	const {fillRect} = getBoard(canvas);
 	const sock = io();
 
+	const onClick = (e) => {
+		const { x, y } = getClickCoordinates(canvas, e);
+
+		sock.emit('turn', { x, y });
+	};
+
+
 	sock.on('message', log);
+	sock.on('turn', ({ x, y}) => fillRect(x, y));
 
 	document
 		.querySelector('#chat-form')
 		.addEventListener('submit', onChatSubmitted(sock));
+
+	canvas.addEventListener('click', onClick);
 })();
 
--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const io = socketio(server);
 io.on('connection', (sock) => {
 	sock.emit('message', 'You are connected');
 	sock.on('message', text => io.emit('message', text));
+	sock.on('turn', ({ x, y }) => io.emit('turn', { x, y }));
 });
 
 server.on('error', (err) => {
